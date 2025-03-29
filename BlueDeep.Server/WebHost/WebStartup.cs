b2d317075c5a2d519032916dac94b608ad9e8cca
Trofimov Oleg <shokertoe@mail.ru>
2025-03-29 17:50:53 +0300

    add health and routing

--- BlueDeep.Server/WebHost/WebStartup.cs
+++ BlueDeep.Server/WebHost/WebStartup.cs
@@ -2,6 +2,11 @@ using BlueDeep.Server.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BlueDeep.Server.WebHost;
 
@@ -11,16 +16,33 @@ public class WebStartup
     {
         services.AddScoped<MessageBrokerService>();
         services.AddScoped<TopicService>();
-        services.AddScoped<WebStatService>();
-    }  
+        services.AddSingleton<WebStatService>();
+        services.AddRouting();
+    }   
     
-    public void Configure(IApplicationBuilder app, WebStatService webStatService)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, WebStatService webStatService)
     {
-        app.UseHttpsRedirection();
-        app.Run(async (context) =>
+        if (env.IsDevelopment())
         {
-            var msg = webStatService.GetWebStatistics();
-            await context.Response.WriteAsync(msg);
-        });
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+
+        app.MapWhen((HttpContext context) => context.Request.Method == "GET" && context.Request.Path == "/",
+            (IApplicationBuilder builder) => {
+                builder.Run(async (context) => {
+                    await context.Response.WriteAsync(webStatService.GetWebStatistics());
+                });
+            });
+        
+        app.MapWhen((HttpContext context) => context.Request.Method == "GET" && context.Request.Path.Value.ToLower() == "/health",
+            (IApplicationBuilder builder) => {
+                builder.Run(async (context) => {
+                    await context.Response.WriteAsync("OK");
+                });
+            });
     }  
 }
